8d6132434900d6cb93ba65b2c139f0fe632608c2
nikoloznadiradze23-prog <nikoloznadiradze23@gmail.com>
2025-10-15 19:46:44 +0400

    commit

---
+++ level08/classwork/cw08.py
@@ -0,0 +1,14 @@
+# name = input("enter your name:") 
+# email = input("enter your email:")
+# password = input("enter your password:")
+# hobby = input("enter your hobby:")
+# print = input(name + email + password + hobby)
+
+num1 = input("enter num 1:")
+num2 = input("enter num2:")
+
+num1= int(num1)
+num2= int(num2)
+
+result = num1 + num2
+print("answer: " + str(result))
